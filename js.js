7b2ba4046fec16816e14d0cf1a17297a7caa81c9
clever484 <abdulkareem1862005@gmail.com>
2025-09-09 23:52:18 -0700

    need some js

---
+++ js.js
@@ -0,0 +1,8 @@
+window.onscroll = ()=>
+{
+    let top = window.scrollY;
+    console.log(document.querySelector(".home-main").offsetTop);
+    console.log(document.querySelector(".about-main").offsetTop);
+    console.log(document.querySelector(".education-main").offsetTop);
+    
+}
