c3ca86d17de8d17696c23d708be2252dc318abd1
clever484 <abdulkareem1862005@gmail.com>
2025-09-13 11:43:59 -0700

    done need some tweak

--- js.js
+++ js.js
@@ -1,8 +1,43 @@
+let sections = document.querySelectorAll(".section");
+let navEle = document.querySelectorAll(".nav-ele div");
 window.onscroll = ()=>
 {
-    let top = window.scrollY;
-    console.log(document.querySelector(".home-main").offsetTop);
-    console.log(document.querySelector(".about-main").offsetTop);
-    console.log(document.querySelector(".education-main").offsetTop);
+    sections.forEach(sec =>
+    {
+        let top = window.scrollY;
+        let elePos = sec.offsetTop-100;
+        let eleHei = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+        if(top >= elePos && top < (elePos+eleHei))
+        {
+            navEle.forEach(nav =>
+            {
+                nav.classList.remove("active");
+                document.querySelector(`[onclick="location.href='#${id}'"]`).classList.add("active");
+            }
+            )
+        }
+        document.querySelector(".nav-bar").classList.remove("nav-height");
+        console.log("scrolled");
+    }
+    )
+    // console.log(document.querySelector(".education-main").offsetTop);
     
+}
+document.querySelector('.menu').addEventListener("click",()=>
+{
+    document.querySelector(".nav-bar").classList.toggle("nav-height");
+    event.stopPropagation();
+});
+for(let i=0;i<4;i++)
+{
+    navEle[i].addEventListener("click",()=>
+    {
+        document.querySelector(".nav-bar").classList.toggle("nav-height");
+    });
+    sections[i].addEventListener("click",()=>
+    {
+        document.querySelector(".nav-bar").classList.remove("nav-height");
+        console.log("sections");
+    });
 }
